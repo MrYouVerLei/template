6d0b7a88fdebb6f4a2798f0110b62ef43ef93f2f
Mr. YouVerLeI <eafonin29@gmail.com>
2025-05-25 16:08:44 +0300

    Диплом. React

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import { HeaderWithSearch } from './components/HeaderWithSearch';
+import { Footer } from './components/Footer';
+import { Main } from './components/Main';
+import { BrowserRouter, Switch, Route, Link, useParams } from "react-router-dom";
+import { Header } from './components/Header';
+import { Search } from './components/Search';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/search/:query" component={SearchPage} />
+                <Route path="/" component={HomePage} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
 
-// export default App;
+export default App;
+
+function HomePage() {
+    return (
+        <div className="app">
+            <HeaderWithSearch />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
+
+function SearchPage({ match }: SearchProps) {
+    const { query } = match.params;
+    return (
+        <div className="app">
+            <Header />
+            <Search query={query} />
+            <Footer />
+        </div>
+    );
+}
+
+interface SearchProps {
+    match: {
+        params: {
+            query: string;
+        };
+    };
+}
